43bf7052b4e44c328faf1dcd0d308e9f10d95c68
Kenny <cywsc1349@hotmail.com>
2023-03-13 15:14:55 +0900

    삭제기능추가

--- test.js
+++ test.js
@@ -2,3 +2,7 @@ const $btnAdd = document.getElementById('btnAdd');
 $btnAdd.addEventListener('click', ()=> {
     console.log('클릭');
 })
+const $btnDel = document.getElementById('btnDel');
+$btnDel.addEventListener('click', ()=> {
+    console.log('삭제');
+})
